6ef9048cde31e75acd0b06ca558bd2f07f149d70
XiaoxueChen1218 <150744883+XiaoxueChen1218@users.noreply.github.com>
2023-11-13 14:07:58 -0800

    Update and rename 04.sql to Analyse_Traffic_Source.sql

--- Analyse_Traffic_Source.sql
+++ Analyse_Traffic_Source.sql
@@ -1,5 +1,5 @@
 SELECT 
-	utm_source,
+    utm_source,
     utm_campaign,
     http_referer,
     COUNT(website_session_id) AS sessions
@@ -9,7 +9,7 @@ GROUP BY utm_source, utm_campaign, http_referer
 ORDER BY sessions DESC;
 
 SELECT
-	COUNT(website_session_id) AS sessions,
+    COUNT(website_session_id) AS sessions,
     COUNT(order_id) AS orders,
     COUNT(order_id) / COUNT(website_session_id) AS session_to_order_conv_rate
 FROM website_sessions
@@ -19,7 +19,7 @@ WHERE website_sessions.created_at < '2012-04-14'
     AND utm_campaign = 'nonbrand';
 
 SELECT
-	MIN(DATE(created_at)) AS week_start_date,
+    MIN(DATE(created_at)) AS week_start_date,
     COUNT(DISTINCT website_session_id) AS sessions
 FROM website_sessions
 WHERE created_at BETWEEN '2012-04-15' AND '2012-05-12'
@@ -28,8 +28,8 @@ WHERE created_at BETWEEN '2012-04-15' AND '2012-05-12'
 GROUP BY WEEK(created_at);
 
 SELECT
-	device_type,
-	COUNT(website_session_id) AS sessions,
+    device_type,
+    COUNT(website_session_id) AS sessions,
     COUNT(order_id) AS orders,
     COUNT(order_id) / COUNT(website_session_id) AS session_to_order_conv_rate
 FROM website_sessions
@@ -40,7 +40,7 @@ WHERE website_sessions.created_at < '2012-05-11'
     AND device_type = 'mobile'
 UNION
 SELECT
-	device_type,
+    device_type,
     COUNT(website_session_id) AS sessions,
     COUNT(order_id) AS orders,
     COUNT(order_id) / COUNT(website_session_id) AS session_to_order_conv_rate
@@ -52,7 +52,7 @@ WHERE website_sessions.created_at < '2012-05-11'
     AND device_type = 'desktop';
     
 SELECT
-	MIN(DATE(created_at)) AS week_start_date,
+    MIN(DATE(created_at)) AS week_start_date,
     COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS dtop_sessions,
     COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mob_sessions
 FROM website_sessions
